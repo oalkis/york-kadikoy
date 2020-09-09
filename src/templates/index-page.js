8ae51fdc108203e0e92d1ab663c249572cf2cd6c
ORKUN ALKIŞ <l2208032@std.yildiz.edu.tr>
2020-09-09 17:04:21 +0300

    add subheading

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,40 +4,54 @@ import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Features from "../components/Features";
 
-export const IndexPageTemplate = ({ image, heading, description, intro }) => (
-  <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top`,
-      }}
-    ></div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns" style={{ width: "100%", margin: "0 auto" }}>
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
-);
+export const IndexPageTemplate = ({
+         image,
+         heading,
+         description,
+         intro,
+         subheading,
+       }) => (
+         <div>
+           <div
+             className="full-width-image margin-top-0"
+             style={{
+               backgroundImage: `url(${
+                 !!image.childImageSharp
+                   ? image.childImageSharp.fluid.src
+                   : image
+               })`,
+               backgroundPosition: `top`,
+             }}
+           ></div>
+           <section className="section section--gradient">
+             <div className="container">
+               <div className="section">
+                 <div
+                   className="columns"
+                   style={{ width: "100%", margin: "0 auto" }}
+                 >
+                   <div className="column is-10 is-offset-1">
+                     <div className="content">
+                       <div className="columns">
+                         <div className="column is-12">
+                           <h3 className="has-text-weight-semibold is-size-2">
+                             {heading}
+                           </h3>
+                           <p>{description}</p>
+                           <center>
+                             <h4>{subheading}</h4>
+                           </center>
+                         </div>
+                       </div>
+                       <Features gridItems={intro.blurbs} />
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </section>
+         </div>
+       );
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
